ce7f552538cc6a2f910ecfc0c319ed6ad998cfd7
kennethy <hykenneth@gmail.com>
2021-03-21 22:55:13 -0400

    add rest of ch5 notes

--- ch5/structs/src/main.rs
+++ ch5/structs/src/main.rs
@@ -5,6 +5,21 @@ struct User {
     active: bool,
 }
 
+struct Point(i32, i32, i32);
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+
 fn main() {
     let user1 = User {
         email: String::from("test@test.com"),
@@ -14,4 +29,16 @@ fn main() {
     };
 
     println!("{}", user1.email);
+
+    let point = Point(1, 2, 3);
+    println!("{}", point.0);
+
+    let ret = Rectangle {
+        width: 30,
+        height: 50
+    };
+
+    println!("{:?}", ret);
+    println!("{:#?}", ret); // pretty print
+    println!("area: {}", ret.area());
 }
