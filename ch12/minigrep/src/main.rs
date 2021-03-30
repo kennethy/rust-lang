124ea4031bc8c2228fa5ac6cc6522e922c47b733
kennethy <hykenneth@gmail.com>
2021-03-30 00:18:26 -0400

    add minigrep

---
+++ ch12/minigrep/src/main.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::process;
+use minigrep;
+
+fn main() {
+
+    let args: Vec<String> = env::args().collect();
+
+    let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        println!("app error: {}", e);
+        process::exit(1);
+    }
+}
