935e63c7afec419227d34761e78dfdd9defc0405
Kenneth Yang <hykenneth@gmail.com>
2023-02-13 22:25:07 -0500

    add notes to ch20

--- ch20.multithreaded-web-server/hello/src/lib.rs
+++ ch20.multithreaded-web-server/hello/src/lib.rs
@@ -1,18 +1,15 @@
-use std::sync::mpsc;
-use std::sync::Arc;
-use std::sync::Mutex;
-use std::thread;
-
-pub struct ThreadPool {
-    workers: Vec<Worker>,
-    sender: mpsc::Sender<Message>,
-}
+use std::{sync::{mpsc, Arc, Mutex}, thread};
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
-enum Message {
-    NewJob(Job),
-    Terminate,
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: Option<mpsc::Sender<Job>>,
+}
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl ThreadPool {
@@ -27,75 +24,65 @@ impl ThreadPool {
         assert!(size > 0);
 
         let (sender, receiver) = mpsc::channel();
-
+        let mut workers = Vec::with_capacity(size);
         let receiver = Arc::new(Mutex::new(receiver));
 
-        let mut workers = Vec::with_capacity(size);
-
         for id in 0..size {
+            // Arc allows the receiver to have multiple owners
+            // Mutex ensures only one worker gets a job from the receiver at a time
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers, sender }
+        ThreadPool { workers, sender: Some(sender) }
     }
 
+    // () after FnOnce represents a closure that takes no params
+    // and returns the unit type ()
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-
-        self.sender.send(Message::NewJob(job)).unwrap();
+        self.sender.as_ref().unwrap().send(job).unwrap();
     }
-}
-
-impl Drop for ThreadPool {
-    fn drop(&mut self) {
-        println!("Sending terminate message to all workers.");
-
-        for _ in &self.workers {
-            self.sender.send(Message::Terminate).unwrap();
-        }
-
-        println!("Shutting down all workers.");
-
-        for worker in &mut self.workers {
-            println!("Shutting down worker {}", worker.id);
-
-            if let Some(thread) = worker.thread.take() {
-                thread.join().unwrap();
-            }
-        }
-    }
-}
-
-struct Worker {
-    id: usize,
-    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let message = receiver.lock().unwrap().recv().unwrap();
+
+            let message = receiver.lock().unwrap().recv();
 
             match message {
-                Message::NewJob(job) => {
-                    println!("Worker {} got a job; executing.", id);
-
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
                     job();
                 }
-                Message::Terminate => {
-                    println!("Worker {} was told to terminate.", id);
-
+                Err(_) => {
+                    println!("Worker {id} disconnected, shutting it down.");
                     break;
                 }
             }
         });
 
-        Worker {
-            id,
-            thread: Some(thread),
+
+        Worker { id, thread: Some(thread) }
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for worker in &mut self.workers {
+
+            // dropping a sender closes the channel
+            drop(self.sender.take());
+
+            println!("shutting down worker {}", worker.id);
+
+            // take() on Option takes the Some variant out and leaves None in its place
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
         }
     }
 }
--- ch20.multithreaded-web-server/hello/src/main.rs
+++ ch20.multithreaded-web-server/hello/src/main.rs
@@ -5,6 +5,7 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::thread;
 use std::time::Duration;
+use std::{io::{prelude::*, BufReader}};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -14,37 +15,38 @@ fn main() {
         // represents a single open connection
         let stream = stream.unwrap();
 
+        // use a pool instead to prevent spawning unlimited amount of threads
         pool.execute(|| {
             handle_connection(stream);
         })
     }
+
+    println!("shutting it down");
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader
+        .lines()
+        .next() // only interested in the first item
+        .unwrap() // takes care of the `Option`
+        .unwrap(); // takes care of the `Result`
 
-    let get = b"GET / HTTP/1.1\r\n"; // b"" converts the string into bytes
-    let sleep = b"GET /sleep HTTP/1.1\r\n";
-kk
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else if buffer.starts_with(sleep) {
-        thread::sleep(Duration::from_secs(5));
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    // match does not do automatic referencing and dereferencing
+    // like the equality method does: let x = if a == b { ...
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        },
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
 
     let contents = fs::read_to_string(filename).unwrap();
 
-    let response = format!(
-        "{}\r\nContent-Length: {}\r\n\r\n{}",
-        status_line,
-        contents.len(),
-        contents
-    );
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap(); // calling flush to ensure all bytes are written to the connection
 }
